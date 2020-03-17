7b5d12a3e38c4cc62043f26f58feea65d8acdfb9
Sebastian <1690316+nukedbit@users.noreply.github.com>
2020-03-17 21:28:47 +0100

    create & update endpoints

--- ssupdatecrash.ServiceInterface/Requests.cs
+++ ssupdatecrash.ServiceInterface/Requests.cs
@@ -5,60 +5,12 @@ using ServiceStack.DataAnnotations;
 using ssupdatecrash.ServiceModel;
 
 namespace ssupdatecrash.ServiceInterface {
-    public interface IAudit 
-    {
-        DateTime CreatedDate { get; set; }
-        string CreatedBy { get; set; }
-        string CreatedInfo { get; set; }
-        DateTime ModifiedDate { get; set; }
-        string ModifiedBy { get; set; }
-        string ModifiedInfo { get; set; }
-        DateTime? SoftDeletedDate { get; set; }
-        string SoftDeletedBy { get; set; }
-        string SoftDeletedInfo { get; set; }
-    }
-    
-    public abstract class AuditBase : IAudit
-    {
-        public DateTime CreatedDate { get; set; }
-        [Required]
-        public string CreatedBy { get; set; }
-        [Required]
-        public string CreatedInfo { get; set; }
-
-        public DateTime ModifiedDate { get; set; }
-        [Required]
-        public string ModifiedBy { get; set; }
-        [Required]
-        public string ModifiedInfo { get; set; }
-
-        [Index] //Check if Deleted
-        public DateTime? SoftDeletedDate { get; set; }
-        public string SoftDeletedBy { get; set; }
-        public string SoftDeletedInfo { get; set; }
-    }
-    
- 
-    [AutoPopulate(nameof(IAudit.CreatedDate),  Eval = "utcNow")]
-    [AutoPopulate(nameof(IAudit.CreatedBy),    Eval = "userAuthName")] //or userAuthId
-    [AutoPopulate(nameof(IAudit.CreatedInfo),  Eval = "`${userSession.DisplayName} (${userSession.City})`")]
-    [AutoPopulate(nameof(IAudit.ModifiedDate), Eval = "utcNow")]
-    [AutoPopulate(nameof(IAudit.ModifiedBy),   Eval = "userAuthName")] //or userAuthId
-    [AutoPopulate(nameof(IAudit.ModifiedInfo), Eval = "`${userSession.DisplayName} (${userSession.City})`")]
-    public abstract class CreateAuditBase<Table,TResponse> : ICreateDb<Table>, IReturn<TResponse> {}
-    
- 
-    [AutoPopulate(nameof(IAudit.ModifiedDate), Eval = "utcNow")]
-    [AutoPopulate(nameof(IAudit.ModifiedBy),   Eval = "userAuthName")] //or userAuthId
-    [AutoPopulate(nameof(IAudit.ModifiedInfo), Eval = "`${userSession.DisplayName} (${userSession.City})`")]
-    public abstract class UpdateAuditBase<Table,TResponse> : IUpdateDb<Table>, IReturn<TResponse> {}
-
-
-    public class CreatePerson : CreateAuditBase<Person,CreateResponse> {
+   
+    public class CreatePerson : ICreateDb<Person>, IReturn<CreateResponse> {
         public string Name { get; set; }
     }   
 
-    public class UpdatePerson : UpdateAuditBase<Person,UpdateResponse> {
+    public class UpdatePerson : IUpdateDb<Person>, IReturn<UpdateResponse> {
         public int Id { get; set; }
         public string Name { get; set; }
     }  
@@ -78,6 +30,13 @@ namespace ssupdatecrash.ServiceInterface {
     }
 
 
+    [Route("/create")]
+    public class CreatePersonGateway : IReturn<Person>
+    {
+        public string Name { get; set; }
+    }
+    
+    
     [Route("/update")]
     public class UpdatePersonGateway : IReturn<Person>
     {
@@ -91,9 +50,26 @@ namespace ssupdatecrash.ServiceInterface {
         {
             try
             {
-                var response = await Gateway.SendAsync(new UpdatePerson
+                var response = await Gateway.SendAsync(new UpdatePerson()
                 {
                     Id = request.Id,
+                    Name = request.Name
+                });
+            
+                return response.Result;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+        
+        public async Task<object> Post(CreatePersonGateway request)
+        {
+            try
+            {
+                var response = await Gateway.SendAsync(new CreatePerson()
+                {
                     Name = request.Name
                 });
             
--- ssupdatecrash.ServiceModel/Person.cs
+++ ssupdatecrash.ServiceModel/Person.cs
@@ -4,7 +4,6 @@ namespace ssupdatecrash.ServiceModel {
     public class Person {
         [AutoIncrement]
         public int Id { get; set; }
-
         public string Name {get;set;}
     }
 }
